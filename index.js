d0f4c0e892b4cd0ce4c53c7a21a370dd48694273
joy4563 <joymondal4563@gmail.com>
2023-03-04 22:07:49 +0600

    nearly done with the everything

--- index.js
+++ index.js
@@ -1,174 +1,225 @@
-// fetch data for home page
-
-const fetchData = (dataLimit) => {
-  url = `https://openapi.programming-hero.com/api/ai/tools`;
-  fetch(url)
+function loadData(dataLimit, sorting) {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
     .then((res) => res.json())
-    .then((data) => showAllDetails(data.data.tools, dataLimit));
-  // console.log(data.data.tools[1]);
-};
-
-function showAllDetails(details, dataLimit) {
-  const imageContainer = document.getElementById("details-container");
-  console.log(details[11]);
-  imageContainer.innerHTML = "";
-
-  details = details.slice(0, dataLimit);
-
-  for (const detail of details) {
-    const div = document.createElement("div");
-    div.innerHTML = `
-                <div class="p-5 border border-[#111111]  bg-opacity-10 bg-[#FFFFFF] rounded-xl">
-                  <img class="rounded-xl  lg:h-60" src="${detail.image}" alt="">
-                    <div id="image-id" class="rounded-xl"></div>
-                    <p class="mt-5 mb-3 text-xl font-semibold text-black">Features</p>
-                    <ol class="pl-4 list-decimal font-normal text-xs text-[#585858]">
-                      <li>${detail.features[0]}</li>
-                      <li>${detail.features[1]}</li>
-                      <li>${detail.features[2]}</li>
-                    </ol>
-                    <hr class=" border-[#111111] opacity-20 my-5" />
-                    <div class="flex justify-between items-center">
-                      <div>
-                        <h1 class="text-xl font-semibold text-black mb-3">${detail.name}</h1>
-                        <p class="text-[#585858] font-medium text-sm">
-                          <i class="fa-solid fa-calendar-days"></i> &nbsp${detail.published_in}
-                        </p>
-                      </div>
-                        <button class="bg-[#FEF7F7] h-10 w-10 rounded-full" onClick="clickOnDetails('${detail.id}')"
-          data-modal-target="staticModal"
-          data-modal-toggle="staticModal"
-        type="button" ><i class="fa-solid fa-arrow-right text-[#EB5757]"></i></button>
-                    </div>
-                </div>
-        `;
-    imageContainer.appendChild(div);
-  }
+    .then((data) => displayData(data.data.tools, dataLimit, sorting));
 }
 
-function clickOnDetails(detailID) {
-  console.log(detailID);
-}
-function seeMore(dataLimit) {
-  const seeMoreButton = document.getElementById("see-more");
-  fetchData();
-}
+function displayData(apis, dataLimit, sorting) {
+  const aiContainer = document.getElementById("ai_container");
+  aiContainer.textContent = "";
 
-fetchData(6);
-
-// fetch data for modal
-const fetchModalData = () => {
-  url = `https://openapi.programming-hero.com/api/ai/tool/12`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => showModalData(data.data));
-  // console.log(data.data.tools[1]);
-};
-
-function showModalData(individualData) {
-  // console --------------------
-  // console.log(individualData.features);
-
-  // modal descriptin
-  const modalDescriptin = document.getElementById("modal-description");
-  modalDescriptin.innerText = individualData.description;
-
-  //  --pricing section
-  // basic
-  const basicField = document.getElementById("basic-price");
-  basicField.innerText = `${
-    individualData.pricing == null
-      ? "Free of cost"
-      : individualData.pricing[0].price[0] != "$"
-      ? "Free of cost"
-      : individualData.pricing[0].price
-  }`;
-
-  // pro
-  const proField = document.getElementById("pro-price");
-  // proField.innerText = individualData.pricing[1].price;
-  proField.innerText = `${
-    individualData.pricing == null
-      ? "Free of cost"
-      : individualData.pricing[1].price[0] != "$"
-      ? "Free of cost"
-      : individualData.pricing[1].price
-  }`;
-
-  // enterprise
-  const enterpriseField = document.getElementById("enterprise-price");
-  enterpriseField.innerText = `${
-    individualData.pricing == null
-      ? "Free of cost"
-      : individualData.pricing[2].price
-  }
-  `;
-
-  // feature and intigration
-
-  // feature
-  const featureKey = Object.keys(individualData.features);
-  const featureContainer = document.getElementById("featurs-container");
-  featureContainer.innerHTML = "";
-  for (const key of featureKey) {
-    const list = document.createElement("li");
-    list.innerText = `${individualData.features[key].feature_name}
-    `;
-    featureContainer.appendChild(list);
-  }
-
-  // integration
-  const integrationObject = individualData.integrations;
-  const integrationContainer = document.getElementById("integration-container");
-  integrationContainer.innerHTML = "";
-
-  if (integrationObject == null) {
-    const list = document.createElement("li");
-    list.innerText = "No data found";
-    integrationContainer.appendChild(list);
+  if (dataLimit && apis.length > 6) {
+    apis = apis.slice(0, 6);
+    showAll.classList.remove("d-none");
   } else {
-    const integrationKey = Object.keys(integrationObject);
+    showAll.classList.add("d-none");
+  }
 
-    for (const key of integrationKey) {
-      const list = document.createElement("li");
-      list.innerText = `${individualData.integrations[key]}
-    `;
-      integrationContainer.appendChild(list);
+  if (sorting) {
+    apis.sort((a, b) => {
+      let da = new Date(a.published_in),
+        db = new Date(b.published_in);
+      return da - db;
+    });
+  }
+
+  for (let j = 0; j < apis.length; j++) {
+    const api = apis[j];
+    // console.log(api);
+    let headerId = "features_container" + j;
+    // console.log(headerId);
+    const div1 = document.createElement("div");
+    div1.classList.add("col");
+    div1.innerHTML = `
+        <div class="card p-4">
+            <img src="${api.image}" class="card-img-top rounded-3 mb-3" alt="..." style="height: 300px;">
+            <h4 class="mb-3">Features</h4>
+            <div class="features_container divSize">
+
+            </div>
+            <hr class="mb-3">
+            <h3>${api.name}</h3>
+            <div class="d-flex justify-content-between">
+                <div class="d-flex gap-2">
+                    <i class="fa-solid fa-calendar-days fs-5"></i>
+                    <h5>${api.published_in}</h5>
+                </div>
+                <div>
+                    <button><i class="fa-sharp fa-solid fa-arrow-right" onclick="featureDetails('${api.id}')" data-bs-toggle="modal" data-bs-target="#exampleModal"></i></button>
+                </div>
+            </div>
+        </div>
+        `;
+    aiContainer.appendChild(div1);
+    const features_container =
+      document.getElementsByClassName("features_container");
+    for (let k = 0; k < api.features.length; k++) {
+      features_container[j].id = headerId;
+      const hFive = document.createElement("h5");
+      hFive.innerHTML = `
+            <h5>${k+1}. ${api.features[k]}</h5>
+            `;
+      document.getElementById(headerId).appendChild(hFive);
+    }
+  }
+  showToggle(false);
+}
+
+function featureDetails(id) {
+  const uri = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  fetch(uri)
+    .then((res) => res.json())
+    .then((data) => showParticularItem(data.data));
+}
+
+function showParticularItem(details) {
+  console.log(details.features);
+
+  const obj = details.features;
+  const keys = Object.keys(obj);
+  const arr = details.integrations;
+
+  let input, output, score;
+
+  if (details.input_output_examples == null) {
+    input = "Can you give any example?";
+    output = "No! Not Yet! Take a break!!!";
+  } else {
+    input = details.input_output_examples[0].input;
+    output = details.input_output_examples[0].output;
+  }
+
+  document.getElementById("description").innerText = details.description;
+
+  if (details.pricing == null) {
+    document.getElementById("basicDiv").classList.add("d-none");
+    document.getElementById("basicFree").classList.remove("d-none");
+
+    document.getElementById("proDiv").classList.add("d-none");
+    document.getElementById("proFree").classList.remove("d-none");
+
+    document.getElementById("notContact").classList.remove("d-none");
+    document.getElementById("contact").classList.add("d-none");
+  } else {
+    const basicMonth = details.pricing[0].price;
+    const proMonth = details.pricing[1].price;
+    const contact = details.pricing[2].plan;
+
+    if (basicMonth[0] == "$") {
+      document.getElementById("basic").innerText = basicMonth;
+      document.getElementById("basicDiv").classList.remove("d-none");
+      document.getElementById("basicFree").classList.add("d-none");
+    } else {
+      document.getElementById("basicDiv").classList.add("d-none");
+      document.getElementById("basicFree").classList.remove("d-none");
+    }
+
+    if (proMonth[0] == "$") {
+      document.getElementById("pro").innerText = proMonth;
+      document.getElementById("proDiv").classList.remove("d-none");
+      document.getElementById("proFree").classList.add("d-none");
+    } else {
+      document.getElementById("proDiv").classList.add("d-none");
+      document.getElementById("proFree").classList.remove("d-none");
+    }
+
+    if (contact == "Enterprise") {
+      document.getElementById("notContact").classList.add("d-none");
+      document.getElementById("contact").classList.remove("d-none");
+    } else {
+      document.getElementById("notContact").classList.remove("d-none");
+      document.getElementById("contact").classList.add("d-none");
     }
   }
 
-  // modal image
-  const modalImage = document.getElementById("modal-image");
+  const feaTures = document.getElementById("features");
+  feaTures.textContent = "";
+  const unorderedList = document.createElement("ul");
+  // console.log(obj);
+  for (const key of keys) {
+    const list = document.createElement("li");
+    list.innerHTML = `
+        <li>${obj[key].feature_name}</li>
+        `;
+    unorderedList.appendChild(list);
+  }
+  feaTures.appendChild(unorderedList);
 
-  if (individualData.accuracy.score != null) {
-    modalImage.innerHTML = `
-<p class="font-semibold text-xs text-white bg-[#EB5757] rounded-md py-1 px-3 text-center absolute right-24  lg:top-32 lg:right-28 ">${
-      individualData.accuracy.score * 100
-    }% accuracy</p>
-  <img src="${
-    individualData.image_link[0]
-  }" class="rounded-xl md:w-80 md:h-72" alt="">`;
+  const integrations = document.getElementById("integrations");
+  integrations.textContent = "";
+
+  if (arr == null) {
+    const header_six = document.createElement("h6");
+    header_six.innerHTML = `
+        <h6>No Data Found</h6>
+        `;
+    integrations.appendChild(header_six);
   } else {
-    modalImage.innerHTML = `
-
-  <img src="${individualData.image_link[0]}" class="rounded-xl md:w-80 md:h-72" alt="">`;
+    const unorderedList2 = document.createElement("ul");
+    for (const val of arr) {
+      const list2 = document.createElement("li");
+      list2.innerHTML = `
+            <li>${val}</li>
+            `;
+      unorderedList2.appendChild(list2);
+    }
+    integrations.appendChild(unorderedList2);
   }
 
-  // modal input
-  const inputField = document.getElementById("input-field");
-  inputField.innerText = `${
-    individualData.input_output_examples == null
-      ? "Can you give any example?"
-      : individualData.input_output_examples[0].input
-  }`;
-  // modal output
-  const outputField = document.getElementById("output-field");
-  outputField.innerText = `${
-    individualData.input_output_examples == null
-      ? "No! Not Yet! Take a break!!!"
-      : individualData.input_output_examples[0].output
-  }`;
+  if (details.accuracy.score != null) {
+    score = details.accuracy.score * 100;
+  }
 
-  // modal accuracy
+  const imgSiteDiv = document.getElementById("imgSite");
+  imgSiteDiv.textContent = "";
+  const div1 = document.createElement("div");
+  if (details.accuracy.score == null) {
+    div1.innerHTML = `
+        <img src="${details.image_link[0]}" class="card-img-top p-3" alt="" style="border-radius: 26px;">
+        `;
+    imgSiteDiv.appendChild(div1);
+  } else {
+    div1.innerHTML = `
+        <img src="${details.image_link[0]}" class="card-img-top p-3" alt="" style="border-radius: 26px;">
+        <p class="text-white bg-danger w-25 text-center fw-bold rounded-3 accurate">${score}% accuracy</p>
+        `;
+    imgSiteDiv.appendChild(div1);
+  }
+
+  document.getElementById("input").innerText = input;
+  document.getElementById("output").innerText = output;
 }
-fetchModalData();
+
+function showToggle(b) {
+  const toggle = document.getElementById("loader");
+  if (b) {
+    toggle.classList.remove("d-none");
+  } else {
+    toggle.classList.add("d-none");
+  }
+}
+
+showToggle(true);
+loadData(6);
+let i = 0;
+let j = 0;
+
+document.getElementById("showAll-btn").addEventListener("click", function () {
+  i = 1;
+  showToggle(true);
+  if (j === 1) {
+    loadData("", "sort");
+  } else {
+    loadData();
+  }
+});
+
+document.getElementById("sortDate").addEventListener("click", function () {
+  j = 1;
+  showToggle(true);
+  if (i === 1) {
+    loadData("", "sort");
+  } else {
+    loadData(6, "sort");
+  }
+});
